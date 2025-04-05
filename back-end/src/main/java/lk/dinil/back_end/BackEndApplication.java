bd1bf614f7645989326db31279e05c3b19be45a6
dinildamsith <dinildamsith52@gmail.com>
2025-04-05 16:15:33 +0530

    Backend Project structure done

--- back-end/src/main/java/lk/dinil/back_end/BackEndApplication.java
+++ back-end/src/main/java/lk/dinil/back_end/BackEndApplication.java
@@ -3,6 +3,7 @@ package lk.dinil.back_end;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BackEndApplication {
 
