c4e9a0fa759df48d9c59a3442f9ac6387aca5538
dinildamsith <dinildamsith52@gmail.com>
2025-04-07 10:10:20 +0530

    sign in api connected done

--- front-end/src/app/pages/auth/signin/signIn.component.ts
+++ front-end/src/app/pages/auth/signin/signIn.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import {  FormsModule} from '@angular/forms';
+import { ApiService } from '../../../service/api.service';
+import { Router} from '@angular/router';
+import { API_ENDPOINTS } from '../../../service/api-endpoints';
 
 @Component({
   selector: 'app-signin',
-  standalone: true,
   templateUrl: 'signIn.component.html',
+  imports: [FormsModule]
 })
 
 export class SignInComponent {
+
+  signInData = {
+    username: '',
+    password: ''
+  }
+
+  constructor(private api: ApiService, private router: Router) {}
+
+  // Sign in handle
+  onSignIn() {
+
+    console.log(this.signInData)
+
+    this.api.post(API_ENDPOINTS.AUTH.SIGNIN, this.signInData).subscribe({
+      next: (res:any) => {
+        console.log('Login success', res);
+        localStorage.setItem('token', res.data); // save token if needed
+        this.router.navigate(['/dash-bord']);
+      },
+      error: (err) => {
+        console.error('Login failed', err);
+        alert('Login failed. Please check your email or password.');
+      }
+    });
+
+  }
 
 }
